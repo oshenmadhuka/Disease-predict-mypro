5f1e422625ed7ae5c021f5b280dfd1b9a6dad298
oshen madhuka bhawajith <126389352+oshenmadhuka@users.noreply.github.com>
2024-09-22 21:52:51 +0530

    adding new

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask 
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     # Register the routes blueprint
     from .routes import main  # Ensure that the relative import uses ".routes"
